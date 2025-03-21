6d3f21afcc9b8da8916fe24501886eb06c510320
hdpiscoding <22520339@gm.uit.edu.vn>
2025-03-21 10:14:23 +0700

    initial commit

---
+++ backend/src/main/java/com/flickzy/FlickzyApplication.java
@@ -0,0 +1,13 @@
+package com.flickzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickzyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flickzy/FlickzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flickzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
